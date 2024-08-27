cdca7caf4e5950b9b6259e9f7934ba8e1abe0de7
Limeade <116500605+LimeaidDev@users.noreply.github.com>
2024-08-27 02:40:57 -0400

    Update playbox.bat

--- playbox.bat
+++ playbox.bat
@@ -1,17 +1,32 @@
 @echo off
+:: Set PowerShell execution policy
 powershell.exe -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force"
 
-echo "Createing virtual environment..."
-python -m venv venv
+:: Check if virtual environment exists
+if not exist venv (
+  echo Creating virtual environment...
+  python -m venv venv
+  
+  :: Activate the virtual environment
+  call venv\Scripts\activate
 
-call venv/Scripts/activate
+  :: Check if requirements.txt exists and install dependencies
+  if exist requirements.txt (
+    echo Installing dependencies...
+    pip install -r requirements.txt
+  ) else (
+    echo requirements.txt is missing. Please provide requirements.txt and try again.
+    exit /b 1
+  )
+) else (
+  echo Virtual environment already exists.
+)
 
-echo "Installing dependencies..."
-pip install -r requirements.txt
+:: Activate the virtual environment if it wasn't activated earlier
+call venv\Scripts\activate
 
-:main
+:: Run the Python script
+python main.py
 
-main.py
+:: Wait for user input before closing
 pause
-
-goto main
