401a31e3aa97f406ff8710294802e4fab5a326f1
Limeade <116500605+LimeaidDev@users.noreply.github.com>
2025-02-01 07:13:15 -0500

    Update spigotserver.sh

--- scripts/spigotserver.sh
+++ scripts/spigotserver.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y tmux neofetch default-jre unzip
 
 # Download Spigot
-wget https://download.getbukkit.org/spigot/spigot-1.20.6.jar
+wget https://download.getbukkit.org/spigot/spigot-1.21.jar
 
 # Make server dir
 mkdir ~/server01
@@ -17,7 +17,7 @@ tmux new-session -d -s server01
 tmux set mouse on
 tmux split-window -h
 tmux split-window -t 1 -v
-tmux send-keys -t 0 'java -jar ~/spigot-1.20.6.jar nogui && echo "eula=true" > eula.txt && clear && java -jar ~/spigot-1.20.6.jar nogui' Enter
+tmux send-keys -t 0 'java -jar ~/spigot-1.21.jar nogui && echo "eula=true" > eula.txt && clear && java -jar ~/spigot-1.21.jar nogui' Enter
 tmux send-keys -t 1 'clear && neofetch && echo "Consider donating to my Ko-fi: ko-fi.com/playbox" && echo -e "\nServer IP: " && curl ifconfig.me && echo' Enter
 tmux send-keys -t 2 'clear && htop' Enter
 
